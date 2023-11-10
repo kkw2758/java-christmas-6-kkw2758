d404994ebf02aef3859ddb336c81ea112b1247d8
kkw2758 <dtc3229@gmail.com>
2023-11-11 08:01:52 +0900

    Refactor(Menu) : 멤버 이름 영문 변경 및 한국어 메뉴 이름 필드 추가

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,25 +1,31 @@
 package christmas.domain;
 
 public enum Menu {
-    양송이수프(6000, Category.APPETIZER),
-    타파스(5500, Category.APPETIZER),
-    시저샐러드(8000, Category.APPETIZER),
-    티본스테이크(55000, Category.MAIN),
-    바비큐립(54000, Category.MAIN),
-    해산물파스타(35000, Category.MAIN),
-    크리스마스파스타(25000, Category.MAIN),
-    초코케이크(15000, Category.DESSERT),
-    아이스크림(5000, Category.DESSERT),
-    제로콜라(3000, Category.DRINK),
-    레드와인(60000, Category.DRINK),
-    샴페인(25000, Category.DRINK);
+    MUSHROOM_SOUP("양송이수프", 6000, Category.APPETIZER),
+    TAPAS("타파스", 5500, Category.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8000, Category.APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55000, Category.MAIN),
+    BBQ_RIB("바비큐립", 54000, Category.MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, Category.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, Category.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15000, Category.DESSERT),
+    ICE_CREAM("아이스크림", 5000, Category.DESSERT),
+    ZERO_COLA("제로콜라", 3000, Category.DRINK),
+    RED_WINE("레드와인", 60000, Category.DRINK),
+    CHAMPAGNE("샴페인", 25000, Category.DRINK);
 
+    private final String name;
     private final int price;
     private final Category category;
 
-    Menu(int price, Category category) {
+    Menu(String name, int price, Category category) {
+        this.name = name;
         this.price = price;
         this.category = category;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPrice() {
