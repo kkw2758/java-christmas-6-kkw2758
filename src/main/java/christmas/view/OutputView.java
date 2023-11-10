311824f2ad6246f361271116bb5022069f03ac47
kkw2758 <dtc3229@gmail.com>
2023-11-11 07:38:06 +0900

    Feat(OutputView) : 화면 출력을 담당하는 OutputView 클래스 생성 및 기본 메서드 추가

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+public class OutputView {
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+}
