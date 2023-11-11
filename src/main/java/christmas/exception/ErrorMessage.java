a849db0c364644c4d3cca7854bdc7f4dc1f28db4
kkw2758 <dtc3229@gmail.com>
2023-11-11 16:07:11 +0900

    Feat(ErrorMessage) : 에러 메시지를 관리하는 Enum 클래스 생성

---
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -0,0 +1,21 @@
+package christmas.exception;
+
+public enum ErrorMessage {
+    INVALID_ORDER_INPUT_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ONLY_DRINK_ORDER_ERROR("음료만 주문시, 주문할 수 없습니다."),
+    MAX_MENU_COUNT_ERROR("메뉴는 한번에 최대 %d개까지만 주문할 수 있습니다."),
+    BLANK_INPUT_ERROR("공백이 입력되었습니다."),
+    INVALID_NATURAL_NUMBER_FORMAT_ERROR("자연수 형식으로 입력해주세요."),
+    NAME_NOT_IN_MENU_ERROR("입력하신 메뉴는 메뉴판에 없습니다.");
+
+
+    private final String message;
+    private final String PREFIX = "[ERROR]";
+    ErrorMessage(String message) {
+        this.message = PREFIX + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
