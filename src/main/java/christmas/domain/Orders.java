ac8501f2e3d6c24e6ef3bbf80b7853df0482d12e
kkw2758 <dtc3229@gmail.com>
2023-11-11 15:11:20 +0900

    Feat(Orders) : 한번의 주문에서 메뉴가 20개를 초과하면 예외 처리한다.

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,34 +1,46 @@
 package christmas.domain;
 
 
-import christmas.domain.dto.Order;
+import christmas.domain.dto.OrderDto;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class Orders {
-
+    private final int MAX_MENU_COUNT = 20;
     private final Map<Menu, Integer> orderDetails = new EnumMap<>(Menu.class);
 
 
-    private Orders(List<Order> orders) {
+    private Orders(List<OrderDto> orders) {
         validateOrders(orders);
     }
 
-    public static Orders of(List<Order> orders) {
+    public static Orders of(List<OrderDto> orders) {
         return new Orders(orders);
     }
 
-    private boolean hasDuplicatedMenu(List<Order> orders) {
+    private boolean hasDuplicatedMenu(List<OrderDto> orders) {
         return orders.stream().
-                map(Order::getName)
+                map(OrderDto::getName)
                 .distinct()
                 .count() != orders.size();
     }
 
-    private void validateOrders(List<Order> orders) {
+    private void validateOrders(List<OrderDto> orders) {
         if (hasDuplicatedMenu(orders)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
+    }
+
+    private void validateMenuCount(List<OrderDto> orders) {
+        if (!checkMenuCount(orders)) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한번에 최대 20개까지만 주문할 수 있습니다.");
+        }
+    }
+
+    private boolean checkMenuCount(List<OrderDto> orders) {
+        return orders.stream()
+                .mapToInt(OrderDto::getCount)
+                .sum() <= MAX_MENU_COUNT;
     }
 }
