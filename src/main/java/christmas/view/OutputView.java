363ee3dbe98eaff08cc9beefb6bc05cf8bc5b239
kkw2758 <dtc3229@gmail.com>
2023-11-12 00:47:55 +0900

    Feat(OutputView) : 프로그램 시작 문구를 출력한다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,16 @@
 package christmas.view;
 
 public class OutputView {
+    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static void println(String message) {
         System.out.println(message);
     }
 
     public static void printNewLine() {
         System.out.println();
+    }
+
+    public static void printStartMessage() {
+        println(START_MESSAGE);
     }
 }
