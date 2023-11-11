1dade81372356d4926c9f526b9b6f92fe0435410
kkw2758 <dtc3229@gmail.com>
2023-11-12 00:56:07 +0900

    Feat(OutputView) : 주문 요청 메시지를 출력하는 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,6 +3,7 @@ package christmas.view;
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String DAY_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static void println(String message) {
         System.out.println(message);
     }
@@ -17,5 +18,9 @@ public class OutputView {
 
     public static void printDayRequestMessage() {
         println(DAY_REQUEST_MESSAGE);
+    }
+
+    public static void printOrderRequestMessage() {
+        println(ORDER_REQUEST_MESSAGE);
     }
 }
