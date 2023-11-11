1af1b44023b4c294726e1a7bf163c93f424b9628
kkw2758 <dtc3229@gmail.com>
2023-11-12 00:51:31 +0900

    Feat(OutputView) : 날짜 입력 요청 메시지 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String DAY_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static void println(String message) {
         System.out.println(message);
     }
@@ -12,5 +13,9 @@ public class OutputView {
 
     public static void printStartMessage() {
         println(START_MESSAGE);
+    }
+
+    public static void printDayRequestMessage() {
+        println(DAY_REQUEST_MESSAGE);
     }
 }
