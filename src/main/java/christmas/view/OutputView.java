1c9964368f5255b403546c831f629852cd106f39
kkw2758 <dtc3229@gmail.com>
2023-11-12 15:16:48 +0900

    Feat(OutputView) : 혜택내역을 출력하는 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.domain.Event;
 import christmas.domain.Menu;
 import christmas.domain.Orders;
 import java.util.Map;
@@ -11,8 +12,10 @@ public class OutputView {
     private static final String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String START_PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDERS_FORMAT = "%s %d개";
+    private static final String BENEFITS_FORMAT = "%s: -%,d원";
     private static final String ORDERS_TAG = "<주문 메뉴>";
     private static final String GIFT_MENU_TAG = "<증정 메뉴>";
+    private static final String BENEFITS_INFO_TAG = "<혜택 내역>";
     private static final String TOTAL_PRICE_BEFORE_SALE_TAG = "<할인 전 총주문 금액>";
     private static final String TOTAL_PRICE_BEFORE_SALE = "%,d원";
 
@@ -61,6 +64,17 @@ public class OutputView {
         }
         for (Menu menu : giftMenu.keySet()) {
             printfWithNewLine(ORDERS_FORMAT, menu.getName(), giftMenu.get(menu));
+        }
+    }
+
+    public static void printBenefitsInfo(Map<Event, Integer> benefitsInfo) {
+        println(BENEFITS_INFO_TAG);
+        if (benefitsInfo.size() == 0) {
+            println("없음");
+            return;
+        }
+        for (Event event : benefitsInfo.keySet()) {
+            printfWithNewLine(BENEFITS_FORMAT, event.getName(), benefitsInfo.get(event));
         }
     }
 }
