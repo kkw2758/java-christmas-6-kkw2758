3b971a9320a40ae774d150def6e37eaaebe16f2f
kkw2758 <dtc3229@gmail.com>
2023-11-12 22:52:01 +0900

    Feat(ChristmasController) : 도메인과 View를 연결해주는 Controller 추가

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,31 @@
+package christmas.controller;
+
+import christmas.domain.Day;
+import christmas.domain.EventApplier;
+import christmas.domain.Orders;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasController {
+
+    public void run() {
+        OutputView.printStartMessage();
+        OutputView.printDayRequestMessage();
+        Day day = InputView.inputDay();
+
+        OutputView.printOrderRequestMessage();
+        Orders orders = InputView.inputOrders();
+        OutputView.printStartPreviewMessage(day.getDate().getDayOfMonth());
+
+        OutputView.printOrders(orders);
+        OutputView.printTotalPriceBeforeSale(orders);
+
+        EventApplier eventApplier = EventApplier.of(orders, day);
+        OutputView.printGiftMenu(eventApplier.getGiftMenu());
+        OutputView.printBenefitsInfo(eventApplier.getBenefitInfo());
+        OutputView.printTotalBenefitAmount(eventApplier.calculateTotalBenefitAmount());
+
+        OutputView.printPriceAfterSale(eventApplier.calculatePriceAfterSale(orders.calculatePriceBeforeSale()));
+        OutputView.printEventBadge(eventApplier.calculateTotalBenefitAmount());
+    }
+}
