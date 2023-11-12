47a764d0253f69dd2e960b4d5f744945c6efe53b
kkw2758 <dtc3229@gmail.com>
2023-11-12 11:29:25 +0900

    Feat(OutputView) : 주문할 메뉴를 출력하는 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,16 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import christmas.domain.Orders;
+
 public class OutputView {
     private static final String NEWLINE = "\n";
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String DAY_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String START_PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String ORDERS_FORMAT = "%s %d개";
+    private static final String ORDERS_TAG = "<주문 메뉴>";
 
     public static void println(String message) {
         System.out.println(message);
@@ -30,5 +35,12 @@ public class OutputView {
 
     public static void printStartPreviewMessage(int day) {
         printfWithNewLine(START_PREVIEW_MESSAGE, day);
+    }
+
+    public static void printOrders(Orders orders) {
+        println(ORDERS_TAG);
+        for (Menu orderedMenu : orders.getOrderedMenus()) {
+            printfWithNewLine(ORDERS_FORMAT, orderedMenu.getName(), orders.getOrderCountWithMenu(orderedMenu));
+        }
     }
 }
