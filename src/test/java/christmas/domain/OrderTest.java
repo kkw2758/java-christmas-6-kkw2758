8eb2c39296b88803476902f640d6170cb0aa44e1
kkw2758 <dtc3229@gmail.com>
2023-11-14 20:41:44 +0900

    Feat(OrderTest) : Order 도메인에 대한 테스트 코드 작성

---
+++ src/test/java/christmas/domain/OrderTest.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.exception.ErrorMessage;
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class OrderTest {
+    @DisplayName("주문 입력의 형식(주문 메뉴 - 수량)이 일치하지 않으면 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"티본스테이크-1-2", "1-티본스테이크", "티본스테이크-1.5", "삼각김밥-3", "-"})
+    void getOrderCountWithMenuTest(String order) {
+        //when & then
+        assertThatThrownBy(() -> Order.of(order))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_ORDER_INPUT_ERROR.getMessage());
+    }
+}
