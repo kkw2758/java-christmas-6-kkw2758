99dce88ab438a846e58b4345a7ac99b7c085c39d
kkw2758 <dtc3229@gmail.com>
2023-11-14 18:08:44 +0900

    Refactor(Category) : 사용하지 않는 메서드 및 필드 삭제

--- src/main/java/christmas/domain/Category.java
+++ src/main/java/christmas/domain/Category.java
@@ -1,18 +1,6 @@
 package christmas.domain;
 
 public enum Category {
-    APPETIZER("애피타이저"),
-    MAIN("메인"),
-    DESSERT("디저트"),
-    DRINK("음료");
+    APPETIZER, MAIN, DESSERT, DRINK;
 
-    private final String category;
-
-    Category(String category) {
-        this.category = category;
-    }
-
-    public String getCategory() {
-        return category;
-    }
 }
