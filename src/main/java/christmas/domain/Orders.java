e6efb5556b28a81e96c23fc1bec91f1ca8015d73
kkw2758 <dtc3229@gmail.com>
2023-11-14 12:47:10 +0900

    Refactor(Orders) : Orders 도메인의 생성자 인자값 변경 및 parser 추가
    - List<OrderDto>를 생성자 인자값으로 받다가 OrderDto가 삭제되면서 생성자 인자값 List<Order>로 변경
    - 입력 받은 문자열을 List<Order>로 변경하는 stringToOrderList 추가
    
    -

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,8 +1,8 @@
 package christmas.domain;
 
 
-import christmas.domain.dto.OrderDto;
 import christmas.exception.ErrorMessage;
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -11,17 +11,23 @@ public class Orders {
     private final int MAX_MENU_COUNT = 20;
     private final Map<Menu, Integer> orderDetails = new EnumMap<>(Menu.class);
 
-    private Orders(List<OrderDto> orders) {
+    private Orders(List<Order> orders) {
         validateOrders(orders);
         initOrders(orders);
     }
 
-    public static Orders of(List<OrderDto> orders) {
-        return new Orders(orders);
+    public static Orders of(String orders) {
+        return new Orders(stringToOrderList(orders));
     }
 
-    private void initOrders(List<OrderDto> orders) {
-        for(OrderDto order : orders) {
+    private static List<Order> stringToOrderList(String orderInput) {
+        return Arrays.stream(orderInput.split(","))
+                .map(Order::of)
+                .toList();
+    }
+
+    private void initOrders(List<Order> orders) {
+        for(Order order : orders) {
             orderDetails.put(Menu.findMenuByName(order.getName().getValue()), order.getCount().getValue());
         }
     }
@@ -50,19 +56,19 @@ public class Orders {
                 .sum();
     }
 
-    private void validateOrders(List<OrderDto> orders) {
+    private void validateOrders(List<Order> orders) {
         validateMenuCount(orders);
         validateDuplicateMenu(orders);
         validateOnlyDrink(orders);
     }
 
-    private void validateOnlyDrink(List<OrderDto> orders) {
+    private void validateOnlyDrink(List<Order> orders) {
         if (checkOnlyDrink(orders)) {
             throw new IllegalArgumentException(ErrorMessage.ONLY_DRINK_ORDER_ERROR.getMessage());
         }
     }
 
-    private boolean checkOnlyDrink(List<OrderDto> orders) {
+    private boolean checkOnlyDrink(List<Order> orders) {
         return orders.stream()
                 .map((order) -> Menu.findMenuByName(order.getName().getValue()))
                 .map(Menu::getCategory)
@@ -71,30 +77,30 @@ public class Orders {
                 .equals(List.of(Category.DRINK));
     }
 
-    private void validateDuplicateMenu(List<OrderDto> orders) {
+    private void validateDuplicateMenu(List<Order> orders) {
         if (hasDuplicatedMenu(orders)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER_INPUT_ERROR.getMessage());
         }
     }
 
-    private boolean hasDuplicatedMenu(List<OrderDto> orders) {
+    private boolean hasDuplicatedMenu(List<Order> orders) {
         return orders.stream().
-                map(OrderDto::getName)
+                map(Order::getName)
                 .map(Name::getValue)
                 .distinct()
                 .count() != orders.size();
     }
 
-    private void validateMenuCount(List<OrderDto> orders) {
+    private void validateMenuCount(List<Order> orders) {
         if (!checkMenuCount(orders)) {
             throw new IllegalArgumentException(
                     String.format(ErrorMessage.MAX_MENU_COUNT_ERROR.getMessage(), MAX_MENU_COUNT));
         }
     }
 
-    private boolean checkMenuCount(List<OrderDto> orders) {
+    private boolean checkMenuCount(List<Order> orders) {
         return orders.stream()
-                .map(OrderDto::getCount)
+                .map(Order::getCount)
                 .mapToInt(Count::getValue)
                 .sum() <= MAX_MENU_COUNT;
     }
