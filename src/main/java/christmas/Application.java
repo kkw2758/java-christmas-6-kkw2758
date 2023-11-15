f183c85687b509d74ef8c0470fd8c6b95279e407
kkw2758 <dtc3229@gmail.com>
2023-11-15 19:32:40 +0900

    Feat(Application) : main 메서드에 Console.close()추가
    - Console.readLine()을 작성할 때 Scanner 객체를 생성하는 코드 확인
    - 입출력 관련 객체들은 반드시 사용 후 닫아줘야한다고 알고 있어서, Console.close() 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,9 +1,11 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.ChristmasController;
 
 public class Application {
     public static void main(String[] args) {
         new ChristmasController().run();
+        Console.close();
     }
 }
