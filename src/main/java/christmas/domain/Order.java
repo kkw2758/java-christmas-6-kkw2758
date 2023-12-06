1923e6ac43e55c6f0d0b25fcfb13b49d165e55ba
kkw2758 <dtc3229@gmail.com>
2023-12-06 21:09:08 +0900

    Feat(Order) : 메뉴-수량을 관리하는 Order 도메인 추가

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+
+import christmas.dto.request.OrderRequest;
+
+public class Order {
+
+    private final Menu menu;
+    private final Count count;
+
+    private Order(Menu menu, Count count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public static Order from(OrderRequest orderRequest) {
+        return new Order(Menu.from(orderRequest.menu()), Count.from(orderRequest.count()));
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Count getCount() {
+        return count;
+    }
+}
