ed8b919cb6625e12aa8fd3ca726f14e9d46d20b2
kkw2758 <dtc3229@gmail.com>
2023-12-06 21:11:38 +0900

    Feat(Orders) : 메뉴-수량 리스트를 관리하는 Orders 도메인 추가 및 예외 처리 기능 추가

---
+++ src/main/java/christmas/domain/Orders.java
@@ -0,0 +1,61 @@
+package christmas.domain;
+
+import christmas.dto.request.OrdersRequest;
+import christmas.exception.ErrorMessage;
+import christmas.utils.Validator;
+import java.util.List;
+
+public class Orders {
+    private static final int MIN_TOTAL_MENU_COUNT = 1;
+    private static final int MAX_TOTAL_MENU_COUNT = 20;
+    private final List<Order> orders;
+
+    private Orders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public static Orders from(OrdersRequest ordersRequest) {
+        List<Order> orders = ordersRequest.orders().stream()
+                .map(Order::from)
+                .toList();
+        validateOrders(orders, ErrorMessage.INVALID_ORDERS);
+        return new Orders(orders);
+    }
+
+    private static void validateOrders(List<Order> orders, ErrorMessage e) {
+        Validator.validateNoDuplicates(convertOrdersToMenu(orders), e);
+        validateNotOnlyDrink(orders, e);
+        Validator.validateInRange(calculateTotalMenuCount(orders), MIN_TOTAL_MENU_COUNT, MAX_TOTAL_MENU_COUNT, e);
+    }
+
+    private static void validateNotOnlyDrink(List<Order> orders, ErrorMessage e) {
+        if (isOrdersHasOnlyDrink(orders)){
+            e.throwException();
+        }
+    }
+
+    private static boolean isOrdersHasOnlyDrink(List<Order> orders) {
+        return convertOrdersToMenuCategories(orders).equals(List.of(MenuCategory.DRINK));
+    }
+
+    private static int calculateTotalMenuCount(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getCount)
+                .map(Count::getValue)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+    private static List<MenuCategory> convertOrdersToMenuCategories(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getMenu)
+                .map(Menu::getCategory)
+                .distinct()
+                .toList();
+    }
+
+    private static List<Menu> convertOrdersToMenu(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getMenu)
+                .toList();
+    }
+}
