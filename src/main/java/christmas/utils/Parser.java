ebe597413b66fb1c2f1d4ab5e470dae7e9c3c6e4
kkw2758 <dtc3229@gmail.com>
2023-12-07 12:30:38 +0900

    Refactor : 입력 받은 값을 RequestDto로 변경하는 메서드 이동

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -1,18 +1,31 @@
 package christmas.utils;
 
 
+import christmas.dto.request.OrderRequest;
+import christmas.dto.request.OrdersRequest;
 import java.util.List;
 
 public class Parser {
+    private static final String COMMA_DELIMITER = ",";
+    private static final String DASH_DELIMITER = "-";
+    private static final int MENU_INDEX = 0;
+    private static final int COUNT_INDEX = 1;
+
     public static List<String> split(final String string, final String regex) {
         return List.of(
                 string.split(regex, -1)
         );
     }
 
-    private static List<Integer> parseStringListToIntegerList(List<String> strings){
-        return strings.stream()
-                .map(Integer::parseInt)
-                .toList();
+    public static OrdersRequest generateOrdersRequest(String orders) {
+        return new OrdersRequest(Parser.split(orders, COMMA_DELIMITER).stream()
+                .map(Parser::generateOrderRequest)
+                .toList());
+    }
+
+    private static OrderRequest generateOrderRequest(String order) {
+        String menu = Parser.split(order, DASH_DELIMITER).get(MENU_INDEX);
+        String count = Parser.split(order, DASH_DELIMITER).get(COUNT_INDEX);
+        return new OrderRequest(menu, Integer.parseInt(count));
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,6 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.dto.request.OrderRequest;
 import christmas.dto.request.OrdersRequest;
 import christmas.exception.ErrorMessage;
 import christmas.utils.Parser;
@@ -10,11 +9,12 @@ import christmas.utils.Validator;
 public class InputView {
     private static final String COMMA_DELIMITER = ",";
     private static final String DASH_DELIMITER = "-";
-    private static final int MENU_INDEX = 0;
     private static final int COUNT_INDEX = 1;
     private static final int ORDER_SIZE = 2;
 
     private static final InputView instance = new InputView();
+    private static final String ORDERS_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String VISIT_DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     private InputView() {
     }
@@ -24,7 +24,7 @@ public class InputView {
     }
 
     public int requestVisitDate(){
-        ConsoleWriter.printlnMessage("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        ConsoleWriter.printlnMessage(VISIT_DATE_REQUEST_MESSAGE);
         String userInput = Console.readLine();
         validateVisitDate(userInput, ErrorMessage.INVALID_VISIT_DATE_INPUT);
         return Integer.parseInt(userInput);
@@ -36,22 +36,10 @@ public class InputView {
     }
 
     public OrdersRequest requestOrders() {
-        ConsoleWriter.printlnMessage("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        ConsoleWriter.printlnMessage(ORDERS_REQUEST_MESSAGE);
         String userInput = Console.readLine();
         validateOrdersInput(userInput, ErrorMessage.INVALID_ORDERS);
-        return generateOrdersRequest(userInput);
-    }
-
-    private OrdersRequest generateOrdersRequest(String orders) {
-        return new OrdersRequest(Parser.split(orders, COMMA_DELIMITER).stream()
-                .map(this::generateOrderRequest)
-                .toList());
-    }
-
-    private OrderRequest generateOrderRequest(String order) {
-        String menu = Parser.split(order, DASH_DELIMITER).get(MENU_INDEX);
-        String count = Parser.split(order, DASH_DELIMITER).get(COUNT_INDEX);
-        return new OrderRequest(menu, Integer.parseInt(count));
+        return Parser.generateOrdersRequest(userInput);
     }
 
     private static void validateOrdersInput(String ordersInput, ErrorMessage e) {
