b247daaf56e2bb1395b9e4f603408bb1277b1285
kkw2758 <dtc3229@gmail.com>
2023-12-08 09:42:24 +0900

    Refactor(OutputView) : OutputView의 기본 메서드 public static으로 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+
 public class OutputView {
     private static final OutputView instance = new OutputView();
 
@@ -9,20 +10,20 @@ public class OutputView {
     public static OutputView getInstance() {
         return instance;
     }
-    
-    public void printlnMessage(String message) {
+
+    public static void printlnMessage(String message) {
         System.out.println(message);
     }
 
-    public void printlnFormat(String message, Object... args) {
+    public static void printlnFormat(String message, Object... args) {
         printlnMessage(String.format(message, args));
     }
 
-    public void printFormat(String message, Object... args) {
+    public static void printFormat(String message, Object... args) {
         printMessage(String.format(message, args));
     }
 
-    public void printMessage(String message) {
+    public static void printMessage(String message) {
         System.out.print(message);
     }
 }
